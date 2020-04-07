3e9a624a3f2adba449ed296f66689b5934aee442
keys4words <keys4words@gmail.com>
2020-04-07 18:38:23 +0300

    start _media

--- src/js/script.js
+++ src/js/script.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
     validateForms('#consultation form');
     validateForms('#order form');
 
-    $('input[name=phone]').mask("+7(111)111-11-11");
+    $('input[name=phone]').mask("+7(999) 999-9999");
 
 
     $('form').submit(function(e) {
